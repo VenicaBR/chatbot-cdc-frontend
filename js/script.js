437485255e021b5e2ebc9986cc5200252e775db6
Gustavo <lacerdadomingues1@gmail.com>
2025-05-31 11:46:45 -0300

    retornando rota

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let BACKEND_URL = "https://chatbot-consumidor-api.azurewebsites.net/perguntar-openai";
+
 document.addEventListener("DOMContentLoaded", () => {
   const chatHistory = document.getElementById("chat-history");
   const userInput = document.getElementById("user-input");
